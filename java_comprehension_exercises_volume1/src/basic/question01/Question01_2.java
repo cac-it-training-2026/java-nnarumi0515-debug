d3b9b82b49d3921951833c32eab87537594fd22a
narumina <narumina@C010422720.zipangu.cac.co.jp>
2026-04-16 14:48:41 +0900

    question06-4が途中になっています。

--- java_comprehension_exercises_volume1/src/basic/question01/Question01_2.java
+++ java_comprehension_exercises_volume1/src/basic/question01/Question01_2.java
@@ -7,8 +7,17 @@ package basic.question01;
 public class Question01_2 {
 
 	public static void main(String[] args) {
-		
+
 		// TODO:ここに実装
+		int applePrice = 100;
+		String stationery = "鉛筆";
+
+		int bananaPrice = applePrice;
+		String writingUtensilString = stationery;
+
+		System.out.println("リンゴ値段は" + applePrice + "です。");
+		System.out.println("バナナの値段は" + bananaPrice + "です。");
+		System.out.println("この筆記用具は" + writingUtensilString + "です。");
 
 	}
 
--- java_comprehension_exercises_volume1/src/basic/question06/Question06_4.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_4.java
@@ -1,12 +1,29 @@
 package basic.question06;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question06_4 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		// TODO:ここに実装
+		int amountBooks = 0;
+		int sellFlag = 0;
+		int bookPrice = 200;
+
+		System.out.println("本を売却します。");
+
+		do {
+			amountBooks++;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("売却する本の冊数を１つ増やしますか？");
+			System.out.println("はい：０，いいえ：１＞");
+			String answer = reader.readLine();
+			sellFlag = Integer.parseInt(answer);
+
+		} while (sellFlag == 0);
 
 	}
 
